1a23878c8ca4d9e99737de5f1ea2104ab0a852a0
mortuz <mortuzalam@gmail.com>
2018-11-08 16:18:33 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CryptoComponent } from './pages/crypto/crypto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'crypto', component: CryptoComponent, pathMatch: 'full'},
+  { path: '', redirectTo: '/crypto', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
